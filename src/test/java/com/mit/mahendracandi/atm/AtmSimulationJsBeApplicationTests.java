d49199580dc2a4be9846c9e5b6c427ca8ad3f326
Candi Mahendra <abdumaadjidcandi@gmail.com>
2026-05-08 00:49:37 +0700

    Initial commit

---
+++ src/test/java/com/mit/mahendracandi/atm/AtmSimulationJsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mit.mahendracandi.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulationJsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
